d5c8f6322ada930395b8272678ae6b5d8e303c29
hardi <hardik.munjaal@gmail.com>
2017-09-06 22:24:29 +0530

    updated corsheaders

--- routes/corsheaders.js
+++ routes/corsheaders.js
@@ -1,4 +1,4 @@
-var allowedHosts = ['http://0.0.0.0:4000','http://swift-dev.drakulaaz.co'];
+var allowedHosts = ['http://0.0.0.0:4000','http://swift-dev.drakulaaz.co','http://localhost:4200'];
 
 
                     
